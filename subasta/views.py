664d7e559fcb886e98cbced5f256c9397ed7f366
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-05-18 10:54:22 -0400

    V1.0.14
    Ver detalle Pasaje

--- subasta/views.py
+++ subasta/views.py
@@ -1,8 +1,20 @@
+from django.urls import reverse_lazy
+from django.views import generic
 from django.shortcuts import render
-from .models import *
-
+from django.views.generic import UpdateView
+from .forms import *
 # Create your views here.
 def ListarSubasta(request):
 	subasta= Subasta.objects.all()
 	context= {'Subastas': subasta}
 	return render(request, 'subasta/subasta_Listar.html', context)
+
+
+def SubastaDetalle(request, pk):
+
+    subasta = Subasta.objects.get(pk=pk) 
+
+    context = {
+        'Subasta': subasta,
+    }
+    return render(request, 'subasta/subasta_verDetalle.html', context)
