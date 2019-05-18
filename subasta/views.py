f2d162e9ea6e45d9a41b6deb5fca0c8ef4156dc1
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-05-18 09:21:47 -0400

    V1.0.8
    Añadido Ver subasta

--- subasta/views.py
+++ subasta/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def ListarSubasta(request):
-    return render(request, 'subasta/subasta_Listar.html')
+	subasta= Subasta.objects.all()
+	context= {'Subastas': subasta}
+	return render(request, 'subasta/subasta_Listar.html', context)
