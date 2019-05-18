ee64bbb233265ca87f0e382777cc7294619a4cfb
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-05-18 12:30:52 -0400

    V1.0.16
    Fix crear subasta solo sin crear

--- subasta/views.py
+++ subasta/views.py
@@ -5,7 +5,6 @@ from django.views.generic import UpdateView
 from .forms import *
 from users.models import Pasaje
 import datetime
-from django.utils.timezone import utc
 # Create your views here.
 def ListarSubasta(request):
 	subasta= Subasta.objects.all()
@@ -23,15 +22,23 @@ def SubastaDetalle(request, pk):
 
 def SubastaAdd(request, pk):
 
-	now = datetime.datetime.utcnow().replace(tzinfo=utc)
+	now = datetime.datetime.now().strftime('%H:%M:%S')
 	pasaje = Pasaje.objects.get(pk=pk)
-	subasta = Subasta.objects.create(ValorSubastaActualizado = pasaje.Valor ,Pasaje_A_Sub = pasaje, HoraI_Subasta = pasaje.Hora_Salida, HoraF_Subasta = pasaje.Hora_Salida, Fecha_Subasta = pasaje.Fecha_Salida
-		, Estado_Subasta = False, Estado_Puja = False
-		, Ultima_Puja = request.user, Puja = 500)
+	Cant_Subasta = Subasta.objects.filter(Pasaje_A_Sub = pasaje).count()
+	
+	if Cant_Subasta == 0:
+		subasta = Subasta.objects.create(ValorSubastaActualizado = pasaje.Valor ,Pasaje_A_Sub = pasaje, HoraI_Subasta = now, HoraF_Subasta = pasaje.Hora_Salida, Fecha_Subasta = pasaje.Fecha_Salida
+			, Estado_Subasta = False, Estado_Puja = False
+			, Ultima_Puja = request.user, Puja = 500)
+		context = {
+		'Subasta': subasta,
+		}
+	else:
+		subasta = Subasta.objects.get(Pasaje_A_Sub = pasaje)
+		context = {
+		'Subasta': subasta,
+		}
 
-	context = {
-	'Subasta': subasta,
-	}
 	return render(request, 'subasta/Est_Subasta.html', context)
 
 
