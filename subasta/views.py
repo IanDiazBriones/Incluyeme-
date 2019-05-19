9bf7b2f26e82169468c1bc1245c819bd16f5639a
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-05-19 18:04:05 -0400

    V1.1.7
    Agregado Tiempos Restantes A subasta

--- subasta/views.py
+++ subasta/views.py
@@ -5,7 +5,9 @@ from django.views.generic import UpdateView
 from .forms import *
 from users.models import Pasaje
 from django.shortcuts import redirect
-import datetime
+from datetime import date
+from datetime import datetime, timedelta
+import smtplib
 # Create your views here.
 def ListarSubasta(request):
 	subasta= Subasta.objects.all()
@@ -15,15 +17,22 @@ def ListarSubasta(request):
 def SubastaDetalle(request, pk):
 
 	subasta = Subasta.objects.get(pk=pk) 
+	time1 = datetime.combine(datetime.now(), subasta.HoraF_Subasta)
+	TiempoMenos5Min = time1 - timedelta(minutes = 5)
+	TiempoRestante =  time1 - datetime.now()
+	if TiempoRestante < timedelta(seconds = 0):
+		TiempoRestante = "Atrasado"
 
 	context = {
 	'Subasta': subasta,
+	'TiempoMenos5Min': TiempoMenos5Min,
+	'TiempoRestante': TiempoRestante
 	}
 	return render(request, 'subasta/subasta_verDetalle.html', context)
 
 def SubastaAdd(request, pk):
 
-	now = datetime.datetime.now().strftime('%H:%M:%S')
+	now = datetime.now().strftime('%H:%M:%S')
 	pasaje = Pasaje.objects.get(pk=pk)
 	Cant_Subasta = Subasta.objects.filter(Pasaje_A_Sub = pasaje).count()
 	
@@ -31,13 +40,30 @@ def SubastaAdd(request, pk):
 		subasta = Subasta.objects.create(ValorSubastaActualizado = pasaje.Valor ,Pasaje_A_Sub = pasaje, HoraI_Subasta = now, HoraF_Subasta = pasaje.Hora_Salida, Fecha_Subasta = pasaje.Fecha_Salida
 			, Estado_Subasta = False, Estado_Puja = False
 			, Ultima_Puja = request.user, Puja = 500)
+		time1 = datetime.combine(datetime.now(), subasta.HoraF_Subasta)
+		TiempoMenos5Min = time1 - timedelta(minutes = 5)
+		TiempoRestante =  time1 - datetime.now()
+		if TiempoRestante < timedelta(seconds = 0):
+			TiempoRestante = "Terminado"
+
 		context = {
 		'Subasta': subasta,
+		'TiempoMenos5Min': TiempoMenos5Min,
+		'TiempoRestante': TiempoRestante
 		}
 	else:
 		subasta = Subasta.objects.get(Pasaje_A_Sub = pasaje)
+		time1 = datetime.combine(datetime.now(), subasta.HoraF_Subasta)
+		TiempoMenos5Min = time1 - timedelta(minutes = 5)
+		TiempoMenos5Min = datetime.time(TiempoMenos5Min)
+		TiempoRestante =  time1 - datetime.now()
+		if TiempoRestante < timedelta(seconds = 0):
+			TiempoRestante = "Terminado"
+
 		context = {
 		'Subasta': subasta,
+		'TiempoMenos5Min': TiempoMenos5Min,
+		'TiempoRestante': TiempoRestante
 		}
 
 	return render(request, 'subasta/Est_Subasta.html', context)
@@ -49,10 +75,17 @@ def SubastaPuja(request, pk):
 
 def SubastaPagar(request, pk):
 
-	subasta = Subasta.objects.get(pk=pk) 
+	subasta = Subasta.objects.get(pk=pk)
+	time1 = datetime.combine(datetime.now(), subasta.HoraF_Subasta)
+	TiempoMenos5Min = time1 - timedelta(minutes = 5)
+	TiempoRestante =  time1 - datetime.now()
+	if TiempoRestante < timedelta(seconds = 0):
+		TiempoRestante = "Atrasado"
 
 	context = {
 	'Subasta': subasta,
+	'TiempoMenos5Min': TiempoMenos5Min,
+	'TiempoRestante': TiempoRestante
 	}
 	return render(request, 'subasta/subasta_Pagar.html', context)
 
