89e88824f2d4549c8c2dfd4fb3d25cf7799d642e
IanDiaz2106 <48776614+IanDiaz2106@users.noreply.github.com>
2019-05-21 16:05:24 -0400

    V1.3.1
    Añadido WebPay

--- pagStatic/urls.py
+++ pagStatic/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('WebPay/<int:pk>/', views.WebPay, name='WebPay'),
+	path('BancoLogin/<int:pk>/', views.BancoLogin, name='BancoLogin'),
 ]
--- pagStatic/views.py
+++ pagStatic/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'pagStatic/index.html')
+
+def WebPay(request, pk):
+	context={'pk':pk}
+	return render(request, 'pagStatic/WebPay.html',context)
+
+def BancoLogin(request, pk):
+	context={'pk':pk}
+	return render(request, 'pagStatic/BancoLogin.html',context)
